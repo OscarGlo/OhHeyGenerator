140a8b6055c039cbce00d2ab26307d2ac0c44bbf
OscarGlo <oscarglo@free.fr>
2020-03-29 18:17:54 +0200

    element copying, 2 way scaling

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
-const elements = {};
+const elements = {}, count = {};
 
 let canvas, ctx, W, H,
     controls;
 
 function addControl(name, x, y, deletable = false) {
+    // Get first possible number
+    let n;
+    if (!count[name]) {
+        count[name] = [];
+    } else {
+        for (let i = 2; i <= 10; i++) {
+            if (!count[name].includes(i)) {
+                count[name].push(i);
+                n = i;
+                break;
+            }
+        }
+        if (!n) return;
+    }
+
+    let baseName = name;
+    if (n) name += n;
+
     let control = document.createElement("div");
     control.id = name;
     control.classList.add("control");
     control.innerHTML = `<label><input type="checkbox"> ${name}</label>`
         + '<span class="addcontrols">'
         + `<label>Position <input name="x" type="number" min="-200" max="200" value="0"><input name="y" type="number" min="-200" max="200" value="0"></label>`
-        + '<label>Scale <input name="scale" type="number" min="0.1" max="5" value="1" step="0.1"></label>'
+        + '<label>Scale <input name="scalex" type="number" min="0.1" max="5" value="1" step="0.1"><input name="scaley" type="number" min="0.1" max="5" value="1" step="0.1"></label>'
         + '<label>Rotation <input name="rotation" type="number" min="-360" max="360" value="0" step="5"></label>'
         + '<button class="reset">Reset</button>'
-        //+ '<button class="add">+</button>'
+        + '<button class="add">+</button>'
         + (deletable ? '<button class="delete">-</button>' : "")
         + '</span>';
 
     controls.appendChild(control);
 
-    let elt = elements[name] = {basex: x, basey: y, x: 0, y: 0, scale: 1, rotation: 0};
+    let elt = elements[name] = {basex: x, basey: y, x: 0, y: 0, scalex: 1, scaley: 1, rotation: 0};
     let img = elt.img = new Image();
-    img.src = "img/" + name + ".png";
+    img.src = "img/" + baseName + ".png";
     elt.show = true;
 
-    let checkbox = document.querySelector(`#${name} > label > input`);
+    let container = document.getElementById(name);
+
+    let checkbox = container.querySelector(`input[type=checkbox]`);
     checkbox.checked = true;
     checkbox.addEventListener("click", () => {
         elt.show = checkbox.checked;
     });
 
-    document.querySelectorAll(`#${name} .addcontrols input`).forEach(param => {
+    container.querySelectorAll(`.addcontrols input`).forEach(param => {
         param.addEventListener("change", () => elt[param.name] = parseFloat(param.value));
     });
 
     // Reset button
-    document.querySelector(`#${name} .reset`).addEventListener("click", () => {
-        Object.assign(elt, {x: 0, y: 0, scale: 1, rotation: 0});
+    container.querySelector(`.reset`).addEventListener("click", () => {
+        Object.assign(elt, {x: 0, y: 0, scalex: 1, scaley: 1, rotation: 0});
 
         // Reset each input to default value
-        document.querySelectorAll(`#${name} .addcontrols input`).forEach(param => {
+        container.querySelectorAll(`.addcontrols input`).forEach(param => {
             param.value = elt[param.name];
         });
     });
 
     // Add button
-    /*document.querySelector(`#${name} .add`).addEventListener("click", () => {
-        addControl(name, x, y, true);
-    });*/
+    container.querySelector(`.add`).addEventListener("click", () => {
+        addControl(baseName, x, y, true);
+    });
+
+    // Add button
+    if (deletable)
+        container.querySelector(`.delete`).addEventListener("click", () => {
+            delete elements[name];
+            container.remove();
+        });
 }
 
 window.addEventListener("load", () => {
@@ -83,7 +110,7 @@ function draw() {
 
             ctx.save();
             ctx.translate(elt.basex + elt.x + w / 2, elt.basey + elt.y + h / 2);
-            ctx.scale(elt.scale, elt.scale);
+            ctx.scale(elt.scalex, elt.scaley);
             ctx.rotate(elt.rotation * 0.01745);
             ctx.drawImage(elt.img, -w / 2, -h / 2);
             ctx.restore();
